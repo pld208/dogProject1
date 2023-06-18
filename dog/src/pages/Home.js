ee78a0450ceae2f32ba4fc02e16e2c064ca3c905
Patrice Williams <patrice@operationspark.org>
2023-06-17 20:38:24 -0500

    added search bar and logo

--- dog/src/pages/Home.js
+++ dog/src/pages/Home.js
@@ -21,9 +21,27 @@ const Home = () => {
 
   return (
     <>
-    {!dogs ? (<h1 className="flex items-center justify-center text-gray-300 text-center px-5 text-3xl">Loading...</h1> ) : (
+    {!dogs ? (<h1 className="flex items-center justify-center text-gray-300 text-center px-5 text-3xl h-screen">Loading...</h1> ) : (
       <>
-        <section>{dogs.length} Pup breeds! </section>
+        <section className="p-8 max-w-6xl mx-auto">
+          <div className="text-center backdrop-blur-sm bg-white/50 border border-orange-800">
+          <h1 className="flex items-center justify-center text-orange-950 text-center px-5 text-5xl font-bold lg:text=5xl pt-6">Search Dogs
+          </h1>
+          <img class="object-top mx-auto px-5 pt-4 " src="./searchDogsLogo.png" alt="Dog logo" width="200" height="300"></img>
+          <p className="my-7">Search Dogs powered by <a href="https://thedogapi.com" className="text-indigo-500 underline active:text-orange-950">The Dog API</a></p>
+          <form className=" mx-auto px-5 pb-8" autoComplete="off">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search for a dog breed..."
+              className="bg-white/50 py-2 px-5 rounded shadow-md w-full pt-3 placeholder-black"
+              />
+          </form>
+          </div>
+          <div>
+          </div>
+        </section>
       </>
     )}
     </>
